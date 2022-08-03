c85b34cd15803ed10eaa2dfa7fc89c11ace64b75
Felipe Cardoso <felipe.cardoso@hotmail.it>
2022-08-03 18:59:09 +0200

    Refactor of LoginPage in HomePage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import type {Page} from "@/types/page";
 import {Login} from "@/components/Login/Login";
 import {MainLayout} from "@/components/Layout/MainLayout";
+import {useAuthConfig} from "@/services/auth";
+import Canvas from "@/components/Canvas/Canvas";
 
-const LoginPage = () => {
-    return (
-        <Login/>
-    );
+const HomePage = () => {
+  const {isPepperLogged} = useAuthConfig();
+  return (
+    isPepperLogged ? <Canvas height={640} width={640}/> : <Login/>
+  );
 };
-LoginPage.getLayout = (page: Page) => <MainLayout>{page}</MainLayout>;
+HomePage.getLayout = (page: Page) => <MainLayout>{page}</MainLayout>;
 
-export default LoginPage;
+export default HomePage;
