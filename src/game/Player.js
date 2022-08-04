b5557d1aa53cdcb9a1dfcd0cb96446a6e621ed8f
Felipe Cardoso <felipe.cardoso@hotmail.it>
2022-08-04 13:28:33 +0200

    Add A and D keys support in game
    close #5

--- src/game/Player.js
+++ src/game/Player.js
@@ -49,25 +49,25 @@ export default class Player {
   }
 
   keydown = (event) => {
-    if (event.code == "ArrowRight") {
+    if (event.code === "ArrowRight" || event.code === "KeyD") {
       this.rightPressed = true;
     }
-    if (event.code == "ArrowLeft") {
+    if (event.code === "ArrowLeft" || event.code === "KeyA") {
       this.leftPressed = true;
     }
-    if (event.code == "Space") {
+    if (event.code === "Space") {
       this.shootPressed = true;
     }
   };
 
   keyup = (event) => {
-    if (event.code == "ArrowRight") {
+    if (event.code === "ArrowRight" || event.code === "KeyD") {
       this.rightPressed = false;
     }
-    if (event.code == "ArrowLeft") {
+    if (event.code === "ArrowLeft" || event.code === "KeyA") {
       this.leftPressed = false;
     }
-    if (event.code == "Space") {
+    if (event.code === "Space") {
       this.shootPressed = false;
     }
   };
