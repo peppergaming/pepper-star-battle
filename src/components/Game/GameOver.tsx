60b61b44bd11ada35d6f9d2bb2b06388e273e82c
Felipe Cardoso <felipe.cardoso@hotmail.it>
2022-08-09 14:53:54 +0200

    Refactor GameOver.tsx

--- src/components/Game/GameOver.tsx
+++ src/components/Game/GameOver.tsx
@@ -15,7 +15,7 @@ interface GameOverProps {
 }
 
 export const GameOver = ({victory, handleReplay, hasNft}: GameOverProps) => {
-  const [claimed, setClaimed] = useState(hasNft);
+  const [claimed, setClaimed] = useState(false);
   /* Change default ship with Ship read onchain #8 */
   const [NFT, setNFT] = useState<Ship>(DEFAULT_SHIP);
   const {userInfo} = useAuthConfig();
@@ -40,64 +40,23 @@ export const GameOver = ({victory, handleReplay, hasNft}: GameOverProps) => {
     )
       .then((response) => response.text())
       .then((result) => {
-        const JSONObject = JSON.parse(result);
-        setNFT(JSONObject[0]);
+        const tokenData = JSON.parse(result)[0];
+        console.debug(tokenData)
+        const attributes = tokenData?.attributes.reduce((map: any, obj: any) => {
+          map[obj.type.toLowerCase()] = obj.value.toLowerCase()
+          return map
+        }, {})
+
+        const ship = new Ship(tokenData.name, tokenData.edition, tokenData.image_url, attributes);
+
+        setNFT(ship);
         setClaimed(true);
       })
       .catch((error) => console.log("error", error));
   };
 
-  const checkIpfs = (value: string) => {
-    if (value.includes("ipfs")) {
-      return value.replace("ipfs://", "https://ipfs.io/ipfs/");
-    } else {
-      return value;
-    }
-  };
-
-  return claimed && victory ? (
-    <Stack alignItems={"center"} direction={"column"}>
-      <Stack
-        direction={"column"}
-        alignItems={"center"}
-        justifyContent={"flex-start"}
-        spacing={1}
-        mt={10}
-      >
-        <Typography variant={"subtitle1"} fontWeight={"bold"} color={"gray"}>
-          Yeeeeeeeh
-        </Typography>
-        <Typography fontSize={32} fontWeight={"bolder"} color={"white"}>
-          You Won {NFT.name}!
-        </Typography>
-        <Typography variant={"subtitle1"} fontWeight={"bold"} color={"white"}>
-          Claimed!
-        </Typography>
-      </Stack>
-      <Stack spacing={2}>
-        <img
-          src={checkIpfs(NFT.getNFTImage())}
-          height={"180px"}
-          width={"180px"}
-          alt={"claimed_ship"}
-          style={{
-            alignSelf: "center",
-            marginTop: "2rem",
-            border: "2px solid gold",
-          }}
-        />
-        <Link href={"https://etherscan.com"}>Check it on EtherScan</Link>
-        <Button
-          sx={{color: "white", marginTop: "10rem"}}
-          endIcon={<ReplayIcon/>}
-          onClick={handleReplay}
-        >
-          Replay
-        </Button>
-      </Stack>
-    </Stack>
-  ) : (
-    <Stack alignItems={"center"} direction={"column"}>
+  if (hasNft || !victory) {
+    return <Stack alignItems={"center"} direction={"column"}>
       <Stack
         direction={"column"}
         alignItems={"center"}
@@ -112,48 +71,102 @@ export const GameOver = ({victory, handleReplay, hasNft}: GameOverProps) => {
           {victory ? "You Won" : "Game Over"}
         </Typography>
       </Stack>
-      {victory ? (
-        <Stack sx={{textAlign: "center"}}>
-          <Typography
-            fontSize={16}
-            sx={{color: "whitesmoke", marginTop: "0"}}
-          >
-            Click on claim to receive it in your wallet
-          </Typography>
-          <img
-            alt={"default_ship"}
-            src={"/images/ship1.jpg"}
-            height={"180px"}
-            width={"180px"}
-            style={{alignSelf: "center", marginTop: "2rem"}}
-          />
-          <Stack mt={6} direction={"column"}>
-            <Button
-              size={"large"}
-              fullWidth
-              variant={"contained"}
-              onClick={claimNFT}
-            >
-              Claim
-            </Button>
-            <Button
-              sx={{color: "white", marginTop: "1rem"}}
-              endIcon={<ReplayIcon/>}
-              onClick={handleReplay}
-            >
-              Replay
-            </Button>
-          </Stack>
-        </Stack>
-      ) : (
+      <Button
+        sx={{color: "white", marginTop: "10rem"}}
+        endIcon={<ReplayIcon/>}
+        onClick={handleReplay}
+      >
+        Replay
+      </Button>
+    </Stack>
+  }
+
+  return claimed ? (
+    <ClaimedSuccess NFT={NFT} handleReplay={handleReplay}/>
+  ) : (
+    <Claim claimNFT={claimNFT} handleReplay={handleReplay}/>
+  );
+};
+
+const Claim = ({claimNFT, handleReplay}) => {
+  return <Stack mt={10} alignItems={"center"} direction={"column"}>
+    <Typography variant={"h3"} fontWeight={"bolder"} color={"white"}>
+      You Won
+    </Typography>
+    <Stack sx={{textAlign: "center"}}>
+      <Typography
+        fontSize={16}
+        sx={{color: "whitesmoke", marginTop: "0"}}
+      >
+        Click on claim to receive a new NFT Ship!
+      </Typography>
+      <img
+        alt={"default_ship"}
+        src={"/images/ship1.jpg"}
+        height={"180px"}
+        width={"180px"}
+        style={{alignSelf: "center", marginTop: "2rem"}}
+      />
+      <Stack mt={6} direction={"column"}>
         <Button
-          sx={{color: "white", marginTop: "10rem"}}
+          size={"large"}
+          fullWidth
+          variant={"contained"}
+          onClick={claimNFT}
+        >
+          Claim
+        </Button>
+        <Button
+          sx={{color: "white", marginTop: "1rem"}}
           endIcon={<ReplayIcon/>}
           onClick={handleReplay}
         >
           Replay
         </Button>
-      )}
+      </Stack>
     </Stack>
-  );
-};
+  </Stack>
+}
+
+const ClaimedSuccess = ({NFT, handleReplay}) => {
+  return <Stack alignItems={"center"} direction={"column"}>
+    <Stack
+      direction={"column"}
+      alignItems={"center"}
+      justifyContent={"flex-start"}
+      spacing={1}
+      mt={10}
+    >
+      <Typography variant={"subtitle1"} fontWeight={"bold"} color={"gray"}>
+        Claimed
+      </Typography>
+      <Typography fontSize={32} fontWeight={"bolder"} color={"white"}>
+        You received a new NFT!
+      </Typography>
+      <Typography variant={"subtitle1"} fontWeight={"bold"} color={"white"}>
+        {NFT.name}
+      </Typography>
+    </Stack>
+    <Stack spacing={2}>
+      <img
+        src={NFT.getNFTImage()}
+        height={"180px"}
+        width={"180px"}
+        alt={"claimed_ship"}
+        style={{
+          alignSelf: "center",
+          marginTop: "2rem",
+          border: "2px solid gold",
+        }}
+      />
+      <Link href={"https://etherscan.com"}>Check it on EtherScan</Link>
+      <Button
+        sx={{color: "white", marginTop: "10rem"}}
+        endIcon={<ReplayIcon/>}
+        onClick={handleReplay}
+      >
+        Replay
+      </Button>
+    </Stack>
+  </Stack>
+}
