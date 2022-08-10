88ec5ee253870243103387f34eafd358f8ff11cd
Felipe Cardoso <felipe.cardoso@hotmail.it>
2022-08-10 10:29:00 +0200

    Remove Modal package

--- src/components/Modal/ModalWrapper/index.tsx
+++
@@ -1,2 +0,0 @@
-export { ModalWrapper } from "./ModalWrapper";
-export type { ModalWrapperProps } from "./ModalWrapper";
--- src/components/Modal/index.tsx
+++
@@ -1,3 +0,0 @@
-export { ModalWrapper } from "./ModalWrapper";
-export { GenericModal } from "./GenericModal";
-export type { ModalWrapperProps } from "./ModalWrapper";
