daa27a1e2a36c07bf8be098854b36ec6a4800593
Felipe Cardoso <felipe.cardoso@hotmail.it>
2022-08-17 16:53:27 +0200

    Update nextjs config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,42 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  frame-ancestors 'self' app.peppergaming.com localhost:3000; 
+`;
+
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/login",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+          },
+        ],
+      },
+      {
+        source: "/oauth",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+          },
+        ],
+      },
+      {
+        key: "X-Frame-Options",
+        value: "allow-from app.peppergaming.com",
+      },
+    ];
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
   reactStrictMode: false,
   swcMinify: false,
   output: 'standalone',
